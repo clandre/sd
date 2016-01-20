51fb69670013500327eae18e0745125c9b42211f
Clandre <claudiondr@ymail.com>
2016-01-20 14:51:22 -0200

    Update ITime.java

--- rmi/src/publishSubscribe/ITime.java
+++ rmi/src/publishSubscribe/ITime.java
@@ -6,5 +6,4 @@ import java.rmi.RemoteException;
 public interface ITime extends Remote {
 	public int getIdade() throws RemoteException;
 	public void subscribe(Client c) throws RemoteException;
-	public void publish() throws RemoteException;
 }
